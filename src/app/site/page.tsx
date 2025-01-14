551fdc388a6c03e9673148c020f27bcfe5c49ab1
fibin J <fibinjohnson123@gmail.com>
2025-01-14 21:32:31 +0530

    checkout

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 
-const Home=()=>{
-    return(
+const Home = () => {
+    return (
+        <>
         <section className="h-full w-full md:pt-44 mt-[-70px] relative flex items-center justify-center flex-col ">
-        {/* grid */}
+            {/* grid */}
 
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" />
-      </section>
+            <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" />
+            <p className="text-center">Run your agency, in one place</p>
+        <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
+          <h1 className="text-9xl font-bold text-center md:text-[300px]">
+            Plura
+          </h1>
+        </div>
+        <div className="flex justify-center items-center relative md:mt-[-70px">
+
+        </div>
+        </section>
+
+        </>
+        
 
     )
 }
